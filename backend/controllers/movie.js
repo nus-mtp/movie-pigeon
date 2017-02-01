fcda74a1b23c0454638a4f170216a2924d8b3bd2
zzzzwen <zhiwen.song.loveslife@gmail.com>
2017-02-01 14:09:47 +0800

    Allow search title through substring

--- backend/controllers/movie.js
+++ backend/controllers/movie.js
@@ -4,7 +4,7 @@ var Movie = require('../models/movie.js');
 // Create endpoint /api/movie for GET
 exports.getMoviesByTitle = function(req, res) {
   // Use the Client model to find all clients
-  Movie.find({ where: {title: req.body.title }}).then(function(movies) {
+  Movie.findAll({ where: {title: { $like: req.body.title } }}).then(function(movies) {
     res.json(movies);
   }).catch(function(err) {
     res.send(err);
