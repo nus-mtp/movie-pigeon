229e33fdab9f70f15d7fc522828ba6b842f936df
zzzzwen <zhiwen.song.loveslife@gmail.com>
2017-02-07 21:25:33 +0800

    Add update function for POST ratings api for existed ratings and Update relevant package

--- backend/controllers/rate.js
+++ backend/controllers/rate.js
@@ -6,17 +6,33 @@ exports.postRates = function (req, res) {
   var movieId = req.body.movieId;
   var score = req.body.score;
   var userId = req.user.id;
-  // Save the rating and check for errors
-  Rate.build({score: score, movie_id: movieId, user_id: userId})
-      .save().then(function (success) {
-        res.json({message: 'Ratings successfully posted!'});
+
+  Rate.find({
+    where: {
+      movie_id: movieId,
+      user_id: userId
+    }
+  }).then(function (rate) {
+    if (rate) {
+      rate.updateAttributes({
+        score: score
+      }).then(function () {
+        return res.json({status: 'success', message: 'Ratings Updated'});
       });
+    } else {
+      // Save the rating and check for errors
+      Rate.build({score: score, movie_id: movieId, user_id: userId})
+        .save().then(function (success) {
+        res.json({status: 'success', message: 'Ratings Posted!'});
+      });
+    }
+  });
 };
 
 // Create endpoint /api/ratings for GET
 exports.getRates = function (req, res) {
   // Use the Ratings model to find all clients
-  Rate.findAll({where: {user_id: req.user.id}}).then(function(ratings){
+  Rate.findAll({where: {user_id: req.user.id}}).then(function (ratings) {
     res.json(ratings);
   });
 };
