81225d599fa9dff8eea09620a2c415888cf5c563
SongYikun <syk950527@gmail.com>
2017-02-13 01:51:07 +0800

    transformer test cases build

--- data/test/test_transformer.py
+++ data/test/test_transformer.py
@@ -4,11 +4,35 @@ from transformer import Transformer
 
 class TestTransformer(unittest.TestCase):
 
+    def __init__(self, *args, **kwargs):
+        super(TestTransformer, self).__init__(*args, **kwargs)
+        self.transformer = Transformer(None)
+
     def test_split_release_and_country_imdb(self):
-        pass
+        self.assertEqual(self.transformer.split_release_and_country_imdb("19 January 2016 (Germany)"),
+                         ("19 January 2016", "Germany"))
+        self.assertEqual(self.transformer.split_release_and_country_imdb("January 2016 (Germany)"),
+                         ("January 2016", "Germany"))
+        self.assertEqual(self.transformer.split_release_and_country_imdb("2016 (Germany)"),
+                         ("2016", "Germany"))
+        self.assertEqual(self.transformer.split_release_and_country_imdb("    2016 (Germany)"),
+                         ("2016", "Germany"))
+        self.assertEqual(self.transformer.split_release_and_country_imdb("    2016     "),
+                         ("2016", None))
 
     def test_transform_date_imdb(self):
-        pass
+        self.assertEqual(self.transformer.transform_date_imdb("19 January 2016"), "2016-01-19")
+        self.assertEqual(self.transformer.transform_date_imdb("1 January 2016"), "2016-01-01")
+        self.assertEqual(self.transformer.transform_date_imdb("01 January 2016"), "2016-01-01")
+        self.assertEqual(self.transformer.transform_date_imdb("February 2016"), "2016-01-01")
+        self.assertEqual(self.transformer.transform_date_imdb("2016"), "2016-01-01")
 
     def test_transform_time_imdb(self):
-        pass
+        self.assertEqual(self.transformer.transform_time_imdb("40min"), "40")
+        self.assertEqual(self.transformer.transform_time_imdb("1h40min"), "100")
+        self.assertEqual(self.transformer.transform_time_imdb("1h"), "60")
+        self.assertEqual(self.transformer.transform_time_imdb("2h40min"), "160")
+        self.assertEqual(self.transformer.transform_time_imdb("1h   40min   "), "100")
+        self.assertEqual(self.transformer.transform_time_imdb("1h   40min"), "100")
+
+
