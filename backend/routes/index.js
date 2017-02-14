b7b3d07b85d8700eaf8525f4f660832ea353de24
zzzzwen <zhiwen.song.loveslife@gmail.com>
2017-02-14 22:34:03 +0800

    Remove redundant code

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -12,43 +12,6 @@ router.route('/users')
   .post(userControl.postUser)
   .get(authController.isAuthenticated, userControl.getUser);
 
-/*
- // on routes that end in /users/:user_id
- // ----------------------------------------------------
- router.route('/users/:user_id')
-
- .put(authController.isAuthenticated, function(req, res) {
- var user = User.build();
-
- user.username = req.body.username;
- user.password = req.body.password;
-
- userControl.updateById(user, req.params.user_id, function(success) {
- console.log(success);
- if (success) {
- res.json({ message: 'User updated!' });
- } else {
- res.send(401, "User not found");
- }
- }, function(error) {
- res.send("User not found");
- });
- })
-
- .get(authController.isAuthenticated, function(req, res) {
- var user = User.build();
-
- userControl.retrieveById(req.params.user_id, function(users) {
- if (users) {
- res.json(users);
- } else {
- res.send(401, "User not found");
- }
- }, function(error) {
- res.send("User not found");
- });
- })
- */
 router.post('/users/login', authController.isAuthenticated, function (req, res) {
   return res.send('login successful');
 });
