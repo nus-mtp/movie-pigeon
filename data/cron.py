9f0eb3831a3b6347397d6029aa5bc68612035d4c
SongYikun <syk950527@gmail.com>
2017-02-26 07:32:40 +0800

    max threads no conflict

--- data/cron.py
+++ data/cron.py
@@ -8,4 +8,7 @@ if __name__ == '__main__':
     scheduler.add_job(processor.update_movie_data, args=[1000000, 2000000, 5])
     scheduler.add_job(processor.update_movie_data, args=[2000000, 3000000, 10])
     scheduler.add_job(processor.update_movie_data, args=[3000000, 4000000, 15])
+    scheduler.add_job(processor.update_movie_data, args=[4000000, 5000000, 20])
+    scheduler.add_job(processor.update_movie_data, args=[5000000, 6000000, 25])
+    scheduler.add_job(processor.update_movie_data, args=[6000000, 7000000, 30])
     scheduler.start()
