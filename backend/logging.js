69f3696720d3e5d1b253897f231fb465bf94ba20
zzzzwen <zhiwen.song.loveslife@gmail.com>
2017-02-26 16:53:16 +0800

    Add error logging

--- backend/logging.js
+++ backend/logging.js
@@ -53,6 +53,60 @@ schedule.scheduleJob(rule, function () {
         console.log(ret);
       });
     }, 1000);
-
   }
 );
+
+var rule2 = new schedule.RecurrenceRule();
+rule2.minute = 59;
+rule2.hour = 9;
+
+schedule.scheduleJob(rule2, function () {
+    pm2.list(function (err, result) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      var data = result;
+      var errored = false;
+      var text = '';
+
+      for (var i in data) {
+        var name = data[i].name;
+        var status = data[i].pm2_env.status;
+        if (!(status === 'online')) {
+          errored = true;
+          text = text + name + ":  " + status + "\r\n";
+        }
+      }
+
+      if (errored) {
+        // setup email data with unicode symbols
+        text = text + new Date().toLocaleString();
+        var mailOptions = {
+          from: '"Movie Pigeon" <cs@movie-pigeon.com>', // sender address
+          to: 'movie.pigeon@gmail.com', // list of receivers
+          subject: '[Movie Pigeon] Server errored', // Subject line
+          text: text, // html body
+          attachments: [
+            {
+              path: '/home/perhaps/.pm2/logs/server-error-0.log'
+            },
+            {
+              path: '/home/perhaps/.pm2/logs/cron-error-0.log'
+            }
+          ]
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            return console.log(error);
+          }
+          console.log('Message %s sent: %s', info.messageId, info.response);
+        });
+      }
+    });
+  }
+);
+
