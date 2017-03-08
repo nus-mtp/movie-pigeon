63874a352b754f9b8f3c93f7d8b6fc0090f994ba
SongYikun <syk950527@gmail.com>
2017-03-08 22:18:14 +0800

    rough restructure

--- data/public_data/controller.py
+++ data/public_data/controller.py
@@ -23,6 +23,50 @@ class ETLController:
 
         self.cinema_list_object = CinemaList()
 
+    def update_movie_data(self, lower, upper, delay):
+        """updates movie data from databases (potentially more than one source)
+            it is a one time process, i.e. data will not be updated constantly
+        """
+        self.logger.info("Initialise movie data retrieval process ...")
+
+        time.sleep(delay)  # delay to avoid conflict
+        existing_movies_id = self.loader.get_movie_id_list()
+
+        for index in range(lower, upper):  # iterate all possible titles
+            imdb_id = utils.imdb_id_builder(index)
+
+            if imdb_id in existing_movies_id:
+                continue
+
+            try:
+                movie_data = self.extractor.extract_movie_data(imdb_id)
+            except error.HTTPError:
+                self.logger.error("Movie ID is not valid." + imdb_id)
+                continue
+            except Exception as e:  # need to find out the exact error type
+                self.logger.error("Movie ID type is not registered." + imdb_id)
+                self.logger.error(e)
+                continue
+
+            try:
+                self.loader.load_movie_data(movie_data)
+            except psycopg2.DataError:
+                self.logger.error("Invalid insertion! Due to the subtext are partially parsed.")
+                continue
+
+        self.logger.info("Movie data update process complete.")
+
+    def update_movie_rating(self):
+        """updates movie rating from popcorn movies (may have to change to raaw implementation in the future)
+        it is a continuous process and data will be updated constantly
+        """
+        self.logger.info("Initialise movie rating update process ...")
+
+        # get list of existing movies
+        id_list = self.loader.get_movie_id_list()
+
+        self.logger.info("Movie rating update process complete.")
+
     def update_cinema_list(self):
         """update cinema list from various theatres websites"""
         # get list
@@ -101,6 +145,45 @@ class ETLController:
             except:
                 print(movie_id)
             self.loader.load_movie_data(data_model.get_movie_data())
+
+class Extractor:
+
+    def __init__(self, logger):
+        self.logger = logger
+
+    @staticmethod
+    def extract_movie_data(movie_id):
+        """given imdb_id, return the metadata of that movie from imdb"""
+        data_model = MovieData(movie_id)
+        data_model.build_soup(data_model.get_html_content())
+        data_model.extract_process()
+        return data_model.get_movie_data()
+
+    @staticmethod
+    def extract_movie_rating(movie_id):
+        """given imdb_id, return a list of dictionaries that contain respective
+        rating and votes from each ratings sources
+        """
+        data_model = MovieRating(movie_id)
+        return data_model.get_movie_ratings()
+
+    @staticmethod
+    def extract_cinema_list():
+        """return a list of dictionaries contains all the cinema names and its
+        respective urls
+        """
+        data_model = CinemaList()
+        final_list = []
+        final_list.extend(data_model._extract_gv_cinema_list())
+        final_list.extend(data_model._extract_cathay_cinema_list())
+        final_list.extend(data_model._extract_sb_cinema_list())
+        return final_list
+
+    @staticmethod
+    def extract_cinema_schedule(cinema):
+        data_model = MovieShowing(cinema)
+        data_model.generic_cinema_extractor()
+        return
 
 if __name__ == '__main__':
     controller = ETLController()
