15b39c116083e91898804996019e3b463ed73614
SongYikun <syk950527@gmail.com>
2017-03-16 23:33:03 +0800

    fixed injection html content bug in movie extractor

--- data/cron.py
+++ data/cron.py
@@ -4,17 +4,17 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import logging
 
 
-# if __name__ == '__main__':
-#     logging.basicConfig(level=logging.WARNING)
-#     scheduler = BlockingScheduler()
-#
-#     controller = ETLController()
-#     scheduler.add_job(controller.update_movie_data, args=[322452, 1000000, 0])
-#     # scheduler.add_job(controller.update_movie_data, args=[1172158, 2000000, 5])
-#     # scheduler.add_job(controller.update_movie_data, args=[2033967, 3000000, 10])
-#     # scheduler.add_job(controller.update_movie_data, args=[3052760, 4000000, 15])
-#
-#     scheduler.start()
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.WARNING)
+    scheduler = BlockingScheduler()
+
+    controller = ETLController()
+    scheduler.add_job(controller.update_movie_data, args=[336913, 1000000, 0])
+    # scheduler.add_job(controller.update_movie_data, args=[1172158, 2000000, 5])
+    # scheduler.add_job(controller.update_movie_data, args=[2033967, 3000000, 10])
+    # scheduler.add_job(controller.update_movie_data, args=[3052760, 4000000, 15])
+
+    scheduler.start()
 
 # #
 # # if __name__ == '__main__':
@@ -22,7 +22,7 @@ import logging
 # #     controller = ETLController()
 # #     controller.update_movie_rating()
 #
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.WARNING)
-    controller = ETLController()
-    controller.update_cinema_schedule()
+# if __name__ == '__main__':
+#     logging.basicConfig(level=logging.WARNING)
+#     controller = ETLController()
+#     controller.update_cinema_schedule()
