d530198004a979b5b9552ae41b4cfbb552004aa4
SongYikun <syk950527@gmail.com>
2017-03-17 19:18:07 +0800

    recomm framework

---
+++ data/recommedation_algo/raw_score.py
@@ -0,0 +1,11 @@
+"""
+    Using multi-linear regression to calculate the weight of each rating
+    source for one user
+"""
+
+class RawScore:
+
+    def __init__(self):
+        pass
+
+    def
---
+++ data/recommedation_algo/recommender.py
@@ -0,0 +1,30 @@
+"""
+    Main class for recommendation operations
+"""
+
+
+class Recommender:
+
+    def __init__(self, user_id):
+        pass
+
+    def run(self):
+        pass
+
+    def update_recommendations(self):
+        """
+        update the general recommendation list in the database
+        :return:
+        """
+        # get relevant
+        # calculate raw score
+        pass
+
+    def get_score(self, movie_id):
+        """
+        get the expected score of a movie
+        :param movie_id:
+        :return:
+        """
+        pass
+
