8d88a2aa7ccdda205f4029413bb519b06e377efd
zzzzwen <zhiwen.song.loveslife@gmail.com>
2017-03-19 01:16:04 +0800

    Change movies details format

--- backend/controllers/utils.js
+++ backend/controllers/utils.js
@@ -4,10 +4,11 @@ module.exports.hasSchedule = function (movies) {
   for (var i in movies) {
     var schedule = movies[i].dataValues.showings;
     if (schedule.length == 0) {
-      movies[i].dataValues.showings = false;
+      movies[i].dataValues.isShowing = false;
     } else {
-      movies[i].dataValues.showings = true;
+      movies[i].dataValues.isShowing = true;
     }
+    delete movies[i].dataValues.showings;
   }
   return movies;
 };
