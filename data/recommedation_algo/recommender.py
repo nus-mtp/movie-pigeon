a07a4fd51ea23157a6d941c6014a5d3b57b01989
SongYikun <syk950527@gmail.com>
2017-03-20 19:12:24 +0800

    added cases for no watching history

--- data/recommedation_algo/recommender.py
+++ data/recommedation_algo/recommender.py
@@ -16,19 +16,15 @@ from database import DatabaseHandler
 from public_data.controller import ETLController
 from sklearn import linear_model
 from datetime import datetime
-from gensim import corpora, models, similarities
-from collections import defaultdict
-
 
 import warnings
 
 
 class Recommender:
 
-    def __init__(self, user_id):
+    def __init__(self):
         self.controller = ETLController()
         self.db = DatabaseHandler()
-        self.user_id = user_id
 
     def run(self):
         # for every user update scale
@@ -113,56 +109,8 @@ class Recommender:
 
             break
 
-
-def test():
-    # Tokenize Corpus and filter out anything that is a
-    # stop word or has a frequency <1
-
-    documents = [
-        'Car Insurance',  # doc_id 0
-        'Car Insurance Coverage',  # doc_id 1
-        'Auto Insurance',  # doc_id 2
-        'Best Insurance',  # doc_id 3
-        'How much is car insurance',  # doc_id 4
-        'Best auto coverage',  # doc_id 5
-        'Auto policy',  # doc_id 6
-        'Car Policy Insurance',  # doc_id 7
-    ]
-
-    stoplist = set(['is', 'how'])
-
-    texts = [[word.lower() for word in document.split()
-              if word.lower() not in stoplist]
-             for document in documents]
-
-    frequency = defaultdict(int)
-    for text in texts:
-        for token in text:
-            frequency[token] += 1
-    texts = [[token for token in text if frequency[token] > 1]
-             for text in texts]
-
-    dictionary = corpora.Dictionary(texts)
-    # doc2bow counts the number of occurences of each distinct word,
-    # converts the word to its integer word id and returns the result
-    # as a sparse vector
-
-    corpus = [dictionary.doc2bow(text) for text in texts]
-    lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
-
-    doc = "giraffe poop car murderer"
-    vec_bow = dictionary.doc2bow(doc.lower().split())
-
-    # convert the query to LSI space
-    vec_lsi = lsi[vec_bow]
-    index = similarities.MatrixSimilarity(lsi[corpus])
-
-    # perform a similarity query against the corpus
-    sims = index[vec_lsi]
-    sims = sorted(enumerate(sims), key=lambda item: -item[1])
-
 if __name__ == '__main__':
     warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")  # ignore lapack related warning
-    test()
-    # recommender = Recommender('8')
-    # recommender.update_user_recommendation()
+    # test()
+    recommender = Recommender('8')
+    recommender.update_user_recommendation()
--- data/recommedation_algo/scale.py
+++ data/recommedation_algo/scale.py
@@ -2,10 +2,57 @@
     Using multi-linear regression to calculate the weight of each rating
     source for one user
 """
+from sklearn import linear_model
+from database import DatabaseHandler
+from data.public_data.controller import ETLController
 
 
-class RawScore:
+class UserScale:
 
-    def __init__(self):
-        pass
+    DEFAULT_WEIGHT = 1 / 3
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.db = DatabaseHandler()
+        self.controller = ETLController()
+
+    def get_user_weight(self):
+        """
+        fit public ratings and user rating to
+        a linear regression model, obtain and
+        return the coefficients
+        :return:
+        """
+        user_rating_records = self.db.get_user_ratings(self.user_id)  # join public rating remove None
+
+        if len(user_rating_records) == 0:  # no user rating / history for this user
+            return self.DEFAULT_WEIGHT, self.DEFAULT_WEIGHT, self.DEFAULT_WEIGHT
+
+        regressors = []
+        responses = []
+        for record in user_rating_records:
+            current_id = record[0]
+
+            # regressors
+            public_rating_records = self.db.get_public_rating(current_id)
+            if not public_rating_records:
+                self.controller.update_single_movie_rating(current_id)  # update rating
+                public_rating_records = self.db.get_public_rating(current_id)
+
+            public_rating_records = sorted(public_rating_records, key=lambda x: x[1])  # sort records -> replace by sql
+            current_set = []
+            for regressor in public_rating_records:
+                current_set.append(regressor[3])
+            regressors.append(current_set)
+
+            # response
+            user_rating = record[1]
+            responses.append(user_rating)
+
+        regression = linear_model.LinearRegression()
+        regression.fit(regressors, responses)
+        return regression.coef_
+
+
+
 
